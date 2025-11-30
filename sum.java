12c533bf69df4e128af10d57553087c89da0b250
Vaishnavi-P1351 <vaishnavideotale86@gmail.com>
2025-11-30 11:42:28 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,14 @@
+public class SumOfTwoNumbers {
+
+    public static void main(String[] args) {
+        // Declare two integer variables
+        int number1 = 10;
+        int number2 = 25;
+
+        // Calculate the sum
+        int sum = number1 + number2;
+
+        // Print the result
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
